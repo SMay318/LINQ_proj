ed0ae98f74396492f0b0d5ad27832508ac13b676
KasebrotII <kasebrot@live.com>
2021-11-17 14:49:03 -0600

    solved problem 6

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -20,7 +20,7 @@ namespace DatabaseFirstLINQ
             ProblemThree();
             ProblemFour();
             ProblemFive();
-            //ProblemSix();
+            ProblemSix();
             //ProblemSeven();
             //ProblemEight();
             //ProblemNine();
@@ -110,8 +110,15 @@ namespace DatabaseFirstLINQ
         {
             // Write a LINQ query that gets all of the users who registered AFTER 2016 and BEFORE 2018
             // Then print each user's email and registration date to the console.
+            var users = _context.Users;
+            var usersBeforeDate = users.Where(u => u.RegistrationDate.Value.Year >= 2016 &&  u.RegistrationDate.Value.Year <= 2018);
+            foreach (User user in usersBeforeDate)
+            {
+                Console.WriteLine($"Email:{user.Email} Registration Date:{user.RegistrationDate}");
+            }
 
         }
+   
 
         // <><><><><><><><> R Actions (Read) with Foreign Keys <><><><><><><><><>
 

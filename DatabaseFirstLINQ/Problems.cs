1ea6e6eab78396baaf63e02c3a98cfff1169b5de
Jacob Haustveit <jacobhaustveit@gmail.com>
2021-11-18 11:32:01 -0500

    Finished problem 18

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -30,9 +30,9 @@ namespace DatabaseFirstLINQ
             //ProblemThirteen();
             //ProblemFourteen();
             //ProblemFifteen();
-            ProblemSixteen();
-            ProblemSeventeen();
-            //ProblemEighteen();
+            //ProblemSixteen();
+            //ProblemSeventeen();
+            ProblemEighteen();
             //ProblemNineteen();
             //ProblemTwenty();
         }
@@ -270,6 +270,9 @@ namespace DatabaseFirstLINQ
         private void ProblemEighteen()
         {
             // Delete the role relationship from the user who has the email "oda@gmail.com" using LINQ.
+            var userRole = _context.UserRoles.Where(ur => ur.User.Email == "oda@gmail.com").SingleOrDefault();
+            _context.UserRoles.Remove(userRole);            
+            _context.SaveChanges();
 
         }
 
